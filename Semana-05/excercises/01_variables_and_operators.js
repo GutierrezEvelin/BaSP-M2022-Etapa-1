a1c88abaf5d1b36cdac613f587753b215a4cf67d
GutierrezEvelin <gutierrezevelinmariagmail.com>
2022-04-16 20:08:05 -0300

    BaSP-2022: ex01-variables and operators

---
+++ Semana-05/excercises/01_variables_and_operators.js
@@ -0,0 +1,22 @@
+//Excercise 1-a
+var num1 = 15;
+var num2 = 30;
+var num3 = num1 + num2;
+
+console.log ("ex01-variables and operators-A:", num3);
+
+//Excercise 1-b
+
+var txt1 = 'Arequito';
+var txt2 = 'Caseros';
+var txt3 = txt1 + ',' + txt2;
+
+console.log ("ex01-variables and operators-B:", txt3);
+
+//Excercise 1-c
+
+var st1 = "Evelin";
+var st2 = "Maria";
+var st3 = st1.length + st2.length;
+
+console.log ("ex01-variables and operators-C:", st3);
